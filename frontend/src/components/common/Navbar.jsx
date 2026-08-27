6ded805901f7f7cfaa335152466a4a264c12e3dd
Kotingo Joseph <YOUR_GITHUB_EMAIL>
2026-08-27 12:53:58 -0400

    Clear user session data on logout

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -12,6 +12,7 @@ export default function Navbar() {
   function logout(){
 
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setMenuOpen(false);
     navigate("/");
 
